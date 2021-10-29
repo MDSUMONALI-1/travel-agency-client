96ceb55c87543ed4db209e7a2cc5316f5e87d937
MDSUMONALI-1 <mdsumonaliceng@gmail.com>
2021-10-29 18:32:42 +0600

    1st

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARbFmTxDs84TN1ScoZ3ob2ez6oDW3TheQ",
+    authDomain: "travel-agaency.firebaseapp.com",
+    projectId: "travel-agaency",
+    storageBucket: "travel-agaency.appspot.com",
+    messagingSenderId: "270240778403",
+    appId: "1:270240778403:web:b3c3759aa115a43cb39716"
+  };
+  export default firebaseConfig;
